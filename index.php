a7056fcef68e378a188b2bdd22a15bfc041d0530
Nicholas Green <nicholasgreen94@gmail.com>
2017-06-03 16:31:58 -0700

    Redoing application with a different design

--- index.php
+++ index.php
@@ -10,24 +10,30 @@
     <link rel="stylesheet" href="stylesheets/styles.css">
   </head>
   <body>
-    <div class="container">
+    <header>
+      <div id="main_header">
+        <div class="container">
+          <h2>Mock Logo</h2>
+        </div>
+      </div>
+    </header>
+    <div id="index" class="container">
       <div class="row">
-        <div id="primary_list" class="five columns">
+        <h1>Your to-do list</h1>
+        <div id="primary_list" class="columns eight">
           <div class="gradient_bar"></div>
-          <img id="profile_image" src="img/user.jpg" alt="a picture of yourself">
-          <h1>Your List</h1>
-          <ul id="list">
-            <li class="list_item"><p>Clean the Dishes</p> <span class="u-pull-right"><b class="delete">&#10005;</b><b class="move_up">&#9720;</b><b class="move_down">&#9727;</b><b class="complete">&#10003;</b></span></li>
-            <li class="list_item"><p>Laundry</p> <span class="u-pull-right"><b class="delete">&#10005;</b><b class="move_up">&#9720;</b><b class="move_down">&#9727;</b><b class="complete">&#10003;</b></span></li>
-            <li class="list_item"><p>Exercise</p> <span class="u-pull-right"><b class="delete">&#10005;</b><b class="move_up">&#9720;</b><b class="move_down">&#9727;</b><b class="complete">&#10003;</b></span></li>
-          </ul>
-          <input type="text" name="user_input" id="add_input">
-          <div id="add">
-            <span>+</span>
+          <div id="add_item">
+            <input type="text" placeholder="Add New Item">
+            <button>Add Item</button>
           </div>
+          <ul>
+            <li>Clean the Dishes <div class="btns"><button class="delete">X</button><button class="up">&gt;</button><button class="down">&lt;</button><button class="complete">&#10004;</button></div></li>
+            <li>Clean the Dishes <div class="btns"><button class="delete">X</button><button class="up">&gt;</button><button class="down">&lt;</button><button class="complete">&#10004;</button></div></li>
+            <li>Clean the Dishes <div class="btns"><button class="delete">X</button><button class="up">&gt;</button><button class="down">&lt;</button><button class="complete">&#10004;</button></div></li>
+          </ul>
         </div>
       </div>
     </div>
   </body>
-  <script type="text/javascript" src="js/scripts.js"></script>
+  <script async defer type="text/javascript" src="js/scripts.js"></script>
 </html>
