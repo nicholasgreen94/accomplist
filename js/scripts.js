9820437ed04a3227da3f5b633ebc96728edef680
Nicholas Green <nicholasgreen94@gmail.com>
2017-06-03 21:20:52 -0700

    Some refactoring, messages item depending on situation

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 
+//> Initial variables
 var primaryList = document.getElementById('primary_list');
 var input = document.querySelector('input[type="text"]');
 var addInput = document.getElementById('add_button');
@@ -9,36 +10,73 @@ var up = document.querySelectorAll('#to_do_list .up');
 var down = document.querySelectorAll('#to_do_list .down');
 var complete = document.querySelectorAll('#to_do_list .complete');
 
+function addMsg(msgType) {
+  var msgValue = msgType;
+  var msgType = document.createElement('p');
+  msgType.className = msgValue + ' message';
+  //> See which value was entered and return corresponding value
+  switch (msgValue) {
+    case 'error': 
+      msgType.textContent = 'Item cannot be blank. Please add an item.';
+      break;
+    case 'added':
+      msgType.textContent = 'The item was successfully added to your list.';
+      break;
+    case 'deleted': 
+      msgType.textContent = 'Your item was successfully removed.';
+      break;
+    case 'success':
+      msgType.textContent = 'Your item was successfully completed.';
+      break;
+  }
+  primaryList.appendChild(msgType);
+  removeMsg(msgType);
+}
+
+//> Create a setTimeout to remove added messages
+function removeMsg(message) {
+  setTimeout(function() {
+    message.remove();
+  }, 5000);
+}
+
 function addNewListItem(e) {
   var newListItem = document.createElement('li');
   var newDiv = document.createElement('div');
   var deleteBtn = document.createElement('button');
   var userInput = input.value;
-  newListItem.textContent = input.value;
-  newDiv.classList.add('btns');
-  deleteBtn.classList.add('delete');
-  deleteBtn.textContent = 'X';
-  newDiv.appendChild(deleteBtn);
-  var upBtn = document.createElement('button');
-  upBtn.classList.add('up');
-  upBtn.textContent = '>';
-  newDiv.appendChild(upBtn);
-  var downBtn = document.createElement('button');
-  downBtn.classList.add('down');
-  downBtn.textContent = '<';
-  newDiv.appendChild(downBtn);
-  var completeBtn = document.createElement('button');
-  completeBtn.classList.add('complete');
-  completeBtn.innerHTML = '&#10003;'; // innerHTML due to HTML entity
-  newDiv.appendChild(completeBtn);
-  newListItem.appendChild(newDiv);
-  toDoList.appendChild(newListItem);
-  input.value = '';
-  // Add a message saying adding the new item was successful
-  var addedItem = document.createElement('p');
-  addedItem.className = 'added message';
-  addedItem.textContent = 'Your item was successfully added to your list';
-  primaryList.appendChild(addedItem);
+  //> Add a new item to the list if the value of the input is not empty
+  if ( input.value  != '' ) {
+    newListItem.textContent = input.value;
+    newDiv.classList.add('btns');
+    //> Add a delete button
+    deleteBtn.classList.add('delete');
+    deleteBtn.textContent = 'X';
+    newDiv.appendChild(deleteBtn);
+    //> Add an up button
+    var upBtn = document.createElement('button');
+    upBtn.classList.add('up');
+    upBtn.textContent = '>';
+    newDiv.appendChild(upBtn);
+    //> Add a down button
+    var downBtn = document.createElement('button');
+    downBtn.classList.add('down');
+    downBtn.textContent = '<';
+    newDiv.appendChild(downBtn);
+    //> Add a complete button
+    var completeBtn = document.createElement('button');
+    completeBtn.classList.add('complete');
+    completeBtn.innerHTML = '&#10003;'; //> innerHTML due to HTML entity
+    newDiv.appendChild(completeBtn);
+    newListItem.appendChild(newDiv);
+    toDoList.appendChild(newListItem);
+    input.value = ''; //> Reset value of the input box to empty
+    //> Add a message saying adding the new item was successful
+    addMsg('added');
+  } else {
+    //> If userInput does not have a value, add error message
+      addMsg('error');
+    }
   e.stopPropagation();
 }
 
@@ -50,24 +88,20 @@ toDoList.addEventListener('click', function(e) {
   var target = e.target;
   if ( target.className == 'delete' ) {
     target.parentNode.parentNode.remove();
-    var deleteMsg = document.createElement('p');
-    deleteMsg.className = 'deleted message';
-    deleteMsg.textContent = 'Your item was successfully removed.';
-    primaryList.appendChild(deleteMsg);
+    //> Show a message saying the item was deleted 
+    addMsg('deleted');
   } else if ( target.className == 'complete' ) {
     target.parentNode.parentNode.remove();
-    var successMsg = document.createElement('p');
-    successMsg.className = 'success message';
-    successMsg.textContent = 'Your item was successfully completed.';
-    primaryList.appendChild(successMsg);
+    //> Message showing the item was successfully completed
+    addMsg('success');
   } else if ( target.className == 'up' ) {
-    var currentElement = target.parentNode.parentNode; // get the list item of the clicked item
-    var parent = currentElement.previousElementSibling; // get the list item above of the clicked one
-    this.insertBefore(currentElement, parent);
+    var currentElement = target.parentNode.parentNode; //> get the list item of the clicked button
+    var parent = currentElement.previousElementSibling; //> get the list item before of the clicked one
+    this.insertBefore(currentElement, parent); //>
   } else if ( target.className == 'down' ) {
-    var currentElement = target.parentNode.parentNode; // get the list item of the clicked item
-    var afterElement = currentElement.nextElementSibling; // get the list item above of the clicked one
-    this.insertBefore(afterElement, currentElement);
+    var currentElement = target.parentNode.parentNode; //> get the list item of the clicked button
+    var afterElement = currentElement.nextElementSibling; //> get the list item above of the clicked one
+    this.insertBefore(afterElement, currentElement); //> Insert currentElement before afterElement
   }
 
 });
